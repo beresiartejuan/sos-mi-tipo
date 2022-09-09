b8833935ae58d98858ecb4b09395ceeed1e3f350
Juan <juanberesiarte@gmail.com>
2022-09-09 00:11:04 -0300

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default function connectDatabase(){
 
-    mongoose.connect(process.env.MONGO_URI).then((db) =>{
+    mongoose.connect(MONGO_URI).then((db) =>{
         console.log(`${db.connection.name} already connect like database!`)
     })
 
